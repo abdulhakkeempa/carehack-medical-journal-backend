4a415cf6b7d893dbac33e51e568a75556af1e05e
akshay k s <95350596+AkuOO4@users.noreply.github.com>
2025-08-03 02:43:52 +0530

    debug patch

--- services/stt.py
+++ services/stt.py
@@ -12,13 +12,14 @@ from groq import Groq
 client = Groq(api_key=os.getenv('GROQ_API_KEY'))
 
 # Specify the path to the audio file
-# filename =  "audio.mp3" # Replace with your audio file!
 
 def whisper_transcribe(file):
     # with open(filename, "rb") as file:
         # Create a transcription of the audio file
+    print("file type: ", type(file))
     transcription = client.audio.transcriptions.create(
         file=file, # Required audio file
+        
         model="whisper-large-v3-turbo", # Required model to use for transcription
         #   prompt="Specify context or spelling",  # Optional
         #   response_format="verbose_json",  # Optional
@@ -29,7 +30,10 @@ def whisper_transcribe(file):
 
     return transcription.text
 
-# print(whisper_transcribe(filename))
+filename =  "audio.mp3" # Replace with your audio file!
+
+
+print(whisper_transcribe(filename))
 
     # To print only the transcription text, you'd use print(transcription.text) (here we're printing the entire transcription object to access timestamps)
 # print(json.dumps(transcription, indent=2, default=str))
